978339e90cc3a0c744f8110c1702cc0172bbb3db
TzuNaMi <150825119+Tzu-nami@users.noreply.github.com>
2025-05-07 11:09:30 +0800

    Update testMain.java

--- testMain.java
+++ testMain.java
@@ -4,11 +4,9 @@ public class testMain {
     public static void main(String[] args) {
         Testing searchEngine = new Testing();
 
-        // Tell it which files to index
         String[] files = {"Doc1.txt", "Doc2.txt", "Doc3.txt"};
         searchEngine.indexDocuments(files);
 
-        // Perform a search
         List<String> results = searchEngine.search("momol");
         System.out.println("Documents: " + results);
     }
